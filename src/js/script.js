bfaf4ae86aa4c5863ee2ea34b20429f117392af4
Victor Candido <victor.candido@iv2.com.br>
2019-09-05 17:41:26 -0300

    Adicionado nova tabela e criado vinculação com servidor node online

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,32 @@
 /**
  * Instancia Planilha Orçamentária
  */
-const tableOrcamentaria = jexcel(document.getElementById("table"), {
-    data: content,
-    columns: Orcamentariacolumns,
-    nestedHeaders: OrcamentarianestedHeaders,
-    minDimensions: [16, 30],
-    toolbar: toolbar,
-    style: style,
-    contextMenu: menuItens,
-    onchange: change_orcamentista
-});
+
+var OrcData = "";
+var tableOrcamentaria;
+
+$.ajax({
+	url:"https://mockup-informov.herokuapp.com/orcamentista/1",
+	type:"GET",
+	success: res => {
+        tableOrcamentaria = jexcel(document.getElementById("table"), {
+            data: JSON.parse(res.data),
+            columns: Orcamentariacolumns,
+            nestedHeaders: OrcamentarianestedHeaders,
+            minDimensions: [16, 10],
+            toolbar: toolbar,
+            style: JSON.parse(res.style),
+            contextMenu: menuItens,
+            onchange: change_orcamentista,
+            updateTable: updateTable
+        });
+        $("#statusServidor").text("Carregado com sucesso!");
+    },
+	error: res => {
+		console.log("Falha", res);
+    }
+})
+
 
 const tableBDI = jexcel(document.getElementById("table-bdi"), {
     data: contentBdi,
@@ -25,16 +41,38 @@ const tableBDI = jexcel(document.getElementById("table-bdi"), {
     contextMenu: menuItens
 });
 
-const calculaK = (valor) => {
+const tableCompat = jexcel(document.getElementById("table-compat"), {
+    data: contentCompat,
+    style: style,
+    minDimensions: [5, 5],
+    columns: columnsCompat,
+    contextMenu: menuItens,
+    toolbar: toolbar,
+    allowInsertColumn: false,
+    allowDeleteColumn: false,
+    allowRenameColumn: false,
+});
+
+const returnKValues = (valor) => {
     var columnK = tableBDI.getColumnData(0);
         
     for(var i in columnK){
         if(columnK[i] == valor){
-            return tableBDI.getRowData(i)[1].split(",").join(".");
+            return { 
+                BDI: tableBDI.getRowData(i)[1].toString().split(",").join("."), 
+                DESC: tableBDI.getRowData(i)[2].toString().split(",").join(".") 
+            }
         }
     }
     return "";
 }
+
+// FUNÇÃO PERSONALIZADA PARA USAR DENTRO DA CELULA
+const TESTE = (v1, v2) => {
+    var value = v1 * tableCompat.getValue(v2);
+    return value
+}
+
 
 /**
  * Criação de funções fora da tabela com jQuery
@@ -51,6 +89,10 @@ $("#fechar").click(e => {
     $("#fechar").hide();
     $("#abrir").show();
 });
+
+// $(window).bind("beforeunload",function(event) {
+//     return "You have unsaved changes";
+// });
 
 $(document).ready(e => {
     $('.tabs').tabs();
